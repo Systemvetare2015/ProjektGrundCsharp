da4c8268ed454245744a75257b74b7e513eae277
Slabadaba <barca_olsen@hotmail.com>
2016-02-12 14:38:51 +0100

    menyskapad
    behöver fixar felix

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,74 +1,81 @@
 ﻿using System;
+using System.Diagnostics;
+using Tetris.Game;
 
 namespace Tetris
 {
     class Program
     {
+        private static string _newLine;
+
         static void Main(string[] args)
         {
-            Console.WriteLine(" test");
-            var exit = false;
-            while (!exit)
-            {
-                
-            var Menyval = MainMeny();
+            MainMeny();
 
-            switch (Menyval)
+
+            var MenyVal = int.Parse(Console.ReadLine());
+
+            switch (MenyVal)
             {
                 case 1:
-
-                    Console.WriteLine(" För att spela krävs en inloggning");
-                    Console.WriteLine(" Välj ett av följande alternativ: ");
-                    Console.WriteLine(" 1. Logga in ");
-                    Console.WriteLine(" 2. Skapa ny användare "+ Environment.NewLine);
-                    
-
-                    var i = Convert.ToInt32(Console.ReadLine());
-
-                    if (i == 1)
-                    {
-                        Console.WriteLine(" Ange dina användaruppgifter! ");
-                        Console.Write(" Ange användarnamn:      ");
-                        string användarnamn = Console.ReadLine();
-                        Console.Write(" Ange lösenord:          ");
-                        string lösenord = Console.ReadLine();
-                        var loggedInUsernam = "";
-                        do
-                        {
-                            loggedInUsernam = LoggaIn(användarnamn, lösenord);
-                        } while (!string.IsNullOrEmpty(loggedInUsernam));
-                    }
-                    else
-                    {
-                        Console.WriteLine(" Ange ett nytt användarnamn och lösenord " + Environment.NewLine);
-
-                        Console.Write(" Ange nytt användarnamn:       ");
-                        string nyttanvändarnamn = Console.ReadLine();
-                        Console.Write(" Ange nytt lösenord            ");
-                        string nyttlösenord = Console.ReadLine();
-                        
-
-
-                        Console.WriteLine(" ===================================");
-                        Console.WriteLine(" ==========    Grattis!   ==========");
-                        Console.WriteLine(" =====  Ditt konto har skapats =====");
-                        Console.WriteLine(" ===================================");
-                        Console.ReadKey();
-                        Console.Clear();
-                     
-                    }
-
+                    Console.Clear();
+                    MenyVal1();
                     break;
 
                 case 2:
-
+                    HighScore();
                     break;
+
             }
-            }
+
 
         }
 
-        public static int MainMeny()
+        public static void MenyVal1()
+        {
+            Console.WriteLine(" =================================");
+            Console.WriteLine(" Välj ett av följande alternativ: ");
+            Console.WriteLine(" =================================" + Environment.NewLine);
+            Console.WriteLine(" 1. Spela ");
+            Console.WriteLine(" 2. Se end credit ");
+
+
+            int menyVal1 = int.Parse(Console.ReadLine());
+
+            switch (menyVal1)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine(" ===========================");
+                    Console.WriteLine("    Ange ett spelarnamn ");
+                    Console.WriteLine(" ===========================" + Environment.NewLine);
+                    Console.Write(" Namn:       ");
+                    string playername = Console.ReadLine();
+                    // starta spel
+                    break;
+
+                case 2:
+                    Console.WriteLine(" ============================");
+                    Console.WriteLine("         END CREDIT          ");
+                    Console.WriteLine(" ============================" + Environment.NewLine);
+                    _newLine = Environment.NewLine;
+                    Console.WriteLine(" Felix Svensson - Gamedesigner" + Environment.NewLine);
+                    Console.WriteLine(" Martin Olsson - Menymaker");
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    MainMeny();
+
+
+                    break;
+
+
+
+
+            }
+        }
+
+        private static void MainMeny()
         {
             Console.WriteLine(" =======================================================");
             Console.WriteLine(" =========           Welcome Player!           =========");
@@ -78,17 +85,22 @@ namespace Tetris
             Console.WriteLine(" =======================================================");
             Console.WriteLine(" ");
             Console.WriteLine(" 1. Logga in och spela   2. Visa Highscore    3. Avsluta ");
-            var returValue = int.Parse(Console.ReadLine());
-            Console.Clear();
-            return returValue;
+
+
         }
 
-        public static string LoggaIn(string Username, string Password)
+        private static void HighScore()
         {
-            var username = "martin";
-            var password = " olsson";
+            Console.Clear();
+            Console.WriteLine("===================== Top 5 Highscores ==================");
+            // anropa highscores
+            Console.ReadKey();
+            MainMeny();
 
-            return null;
         }
+
     }
-}
+
+    }
+
+
