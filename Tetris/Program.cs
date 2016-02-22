b7a627eac4676bcf3422294dbc2811bfe0b50eed
Slabadaba <barca_olsen@hotmail.com>
2016-02-22 16:54:00 +0100

    Nya classer

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -58,6 +58,9 @@ namespace Tetris
                     Console.WriteLine(" ===========================" + Environment.NewLine);
                     Console.Write(" Namn:       ");
                     string playername = Console.ReadLine();
+                    var newPlayer = new Player(playername);
+                    newPlayer.Score = TetrisGame.Play();
+                    newPlayer.Game = Player.GameType.Tetris;
                     // starta spel
                     break;
 
@@ -92,7 +95,7 @@ namespace Tetris
             Console.WriteLine(" =======================================================");
             Console.WriteLine(" ");
             Console.WriteLine(" 1. Logga in och spela   2. Visa Highscore    3. Avsluta ");
-            Console.ReadKey();
+            
 
         }
 
---
+++ Tetris/player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tetris
+{
+    public class Player
+    {
+        public Player(string playername)
+        {
+            PlayerName = PlayerName;
+                        TimePlayed =DateTime.Now;
+        }
+
+        public Player()
+        {
+            
+        }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+        public DateTime TimePlayed { get; set; }
+        public GameType Game { get; set; }
+
+        public enum GameType
+        {
+            Tetris,
+            Snake
+        }
+    }
+}
